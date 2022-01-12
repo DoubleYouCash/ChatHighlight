3196b02e2d980dd63b28f4b0d98d2a30887aa088
wyattd <wyattd@iastate.edu>
2022-01-12 06:50:59 -0600

    Initial Commit

---
+++ src/main/java/ChatHighlight.java
@@ -0,0 +1,15 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ChatHighlight extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+
+}
